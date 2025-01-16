d22468bcfd2218fe4699d6864e26d1b548065b3e
Wedema, D. (Daniël) <daniel.wedema@gmail.com>
2025-01-16 01:34:19 +0100

    Updated auth and deploy configuration

--- src/pega/pega.component.ts
+++ src/pega/pega.component.ts
@@ -8,19 +8,22 @@ import { OAuth2Service } from '@typescale/dx-engine';
     <dx-pega-entry *ngIf="token"
             caseTypeID="ATHO-Insurance-Work-LeningBerekenen"
             [infinityServer]="infinityServer"
+            [deployUrl]="deployUrl"
             [token]="token"></dx-pega-entry>
   `
 })
 export class PegaComponent {
   public token!: TokenInfo;
   public infinityServer!: string;
+  public deployUrl = window.location.origin;
 
   public constructor() {
     this.infinityServer = 'http://localhost:3333/prweb';
-    OAuth2Service.getTokenCredentials({
+    OAuth2Service.getTokenAuthorizationCode({
       serverUrl: this.infinityServer,
-      clientId: '45946986569480875840',
-      clientSecret: '17CA02A7E811F063861884214028D54D'
+      deployUrl: this.deployUrl,
+      clientId: '11680909344524843879',
+      clientSecret: '769E32F2200BCBF7241827FD98D4F22E'
     }).then(token => {
       this.token = token;
     });
